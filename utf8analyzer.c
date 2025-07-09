f8ea21c4802aa5d976dcd37c381d6f1f030ead74
antspham <165956921+antspham@users.noreply.github.com>
2025-07-09 13:20:52 -0700

    Update utf8analyzer.c

--- utf8analyzer.c
+++ utf8analyzer.c
@@ -4,6 +4,39 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+uint32_t utf8_decode(const char input[], int *length) {
+    unsigned char b0 = input[0];
+
+    if ((b0 & 0b10000000) == 0) {
+        *length = 1;
+        return b0 & 0x7F;
+    } else if ((b0 & 0b11100000) == 0b11000000) {
+        unsigned char b1 = input[1];
+        *length = 2;
+        return (((uint32_t)(b0 & 0b00011111)) << 6) |
+               (b1 & 0b00111111);
+    } else if ((b0 & 0b11110000) == 0b11100000) {
+        unsigned char b1 = input[1];
+        unsigned char b2 = input[2];
+        *length = 3;
+        return (((uint32_t)(b0 & 0b00001111)) << 12) |
+               (((uint32_t)(b1 & 0b00111111)) << 6) |
+               (b2 & 0b00111111);
+    } else if ((b0 & 0b11111000) == 0b11110000) {
+        unsigned char b1 = input[1];
+        unsigned char b2 = input[2];
+        unsigned char b3 = input[3];
+        *length = 4;
+        return (((uint32_t)(b0 & 0b00000111)) << 18) |
+               (((uint32_t)(b1 & 0b00111111)) << 12) |
+               (((uint32_t)(b2 & 0b00111111)) << 6) |
+               (b3 & 0b00111111);
+    }
+
+    *length = 1;
+    return 0xFFFD;
+}
+
 void length_of_string(const char str[]) {
 	int length = strlen(str);
 	printf("Length in bytes: %d\n", length);
