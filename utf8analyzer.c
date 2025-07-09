9121dbd9622e705cb967fd4d23ec1fc49e04e5b0
Valerio <jvalerio@ieng6-203.ucsd.edu>
2025-07-09 12:22:07 -0700

    Added UpperCase

--- utf8analyzer.c
+++ utf8analyzer.c
@@ -4,6 +4,16 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+
+void to_uppercase(char str[]) {
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] >= 'a' && str[i] <= 'z') {
+            str[i] = str[i] - 32;
+        }
+    }
+
+
+
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         fprintf(stderr, "Usage: utf8analyzer \"<UTF-8 encoded string>\"\n");
@@ -11,4 +21,7 @@ int main(int argc, char *argv[]) {
     }
 
     // implement the UTF-8 analyzer here
+	
+
+    // Uppercase 
 }
